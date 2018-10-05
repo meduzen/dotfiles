31bad0481871156cbafabf34d4d863a9cfea20f8
Mehdi M <meduzen@gmail.com>
2018-10-05 16:55:42 +0200

    Link Zsh and Mackup settings

--- install.sh
+++ install.sh
@@ -43,11 +43,11 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH/plugins/zsh-
 npm install --global pure-prompt # Minimal ZSH prompt https://github.com/sindresorhus/pure
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-# rm -rf $HOME/.zshrc
-# ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
-# ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
